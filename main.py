355aa74d505b61c62495a6757d40dcdb950b5358
Zoellner122 <r1997@live.nl>
2023-06-21 15:30:31 +0200

    Adding logic for QCOW2 handling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from modules import *
+from modules import mount_handler, qcow2_handler
 import argparse
 import os
 import subprocess
@@ -7,7 +7,8 @@ import sys
 def parse_arguments():
     parser = argparse.ArgumentParser(add_help=True)
     parser.add_argument('--mount', '-m', help="Should OS-Deployer mount a device for you?", action='store_true')
-    parser.add_argument('device', nargs='?', help="Positional argument")
+    parser.add_argument('--device', '-d', nargs=1, help="Positional argument")
+    parser.add_argument('--qcow2', '-q', nargs=1, help="QCOW2 file to deploy")
     return parser
 
 def root_check():
@@ -16,6 +17,7 @@ def root_check():
 
 def main(args, parser):
   root_check()
+  qcow2_handler.qemu_dd(args.qcow2, args.device)
   if args.mount:
       if not args.device:
           print("Error: Positional argument is required when using the --mount option.")

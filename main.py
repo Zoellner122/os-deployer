d1c08706f9d36f7fa733f1dd0d09d9a2a2f6d534
Zoellner122 <r1997@live.nl>
2023-06-21 13:52:26 +0200

    Adding basic root-permission check

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 import subprocess
 import sys
 
-
 def parse_arguments():
     parser = argparse.ArgumentParser(add_help=True)
     parser.add_argument('--mount', '-m', help="Should OS-Deployer mount a device for you?", action='store_true')
@@ -12,20 +11,20 @@ def parse_arguments():
     return parser
 
 def root_check():
-  if not 'SUDO_UID' in os.environ.keys()or os.geteuid() != 0:
-    sys.exit("OS-Deployer requires super-user or root")
- 
+  if not 'SUDO_UID' in os.environ.keys() or os.geteuid() != 0:
+    sys.exit("OS-Deployer needs sudo or root")
+
 def main(args, parser):
-    if args.mount:
-        if not args.device:
-            print("Error: Positional argument is required when using the --mount option.")
-            return
-        mount(args.device)
-    else:
-        parser.print_help()
+  root_check()
+  if args.mount:
+      if not args.device:
+          print("Error: Positional argument is required when using the --mount option.")
+          return
+      check_mount(args.device)
+  else:
+      parser.print_help()
 
 if __name__ == "__main__":
   parser = parse_arguments()
   args = parser.parse_args()
-  root_check()
   main(args, parser)

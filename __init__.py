297720dcf8f0f9f15a0a07191a342aea2a0b676e
Zoellner122 <r1997@live.nl>
2023-06-21 11:49:02 +0200

    Also adding to __init__ for future usage

--- __init__.py
+++ __init__.py
@@ -1,7 +1,12 @@
 # __init__.py
+from .modules.mount_handler import *
+import argparse
+import os
+import subprocess
+import sys
 
 print("Preparing OS-Deployer")
 
-PACKAGE_NAME = "OS-Deployer"
+PACKAGE_NAME = "os-deployer"
 VERSION = "0.1"
 
